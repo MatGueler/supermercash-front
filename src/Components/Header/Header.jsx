56cf9a41b6d2e7239dd1cc0ae74d8e645fdfbdcb
MattGueler <mateus.gueler@gmail.com>
2022-09-30 18:45:35 -0300

    fix: Get user infos when start the menu page and send to header component by props

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,34 +6,8 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-function Header({ loading, setLoading }) {
+function Header({ userInfo }) {
   let navigate = useNavigate();
-
-  const [userInfo, setUserInfo] = useState("");
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    getUserInfo(token);
-  }, []);
-
-  function getUserInfo(token) {
-    const config = {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    };
-    axios
-      .get(`http://localhost:5000/user/me`, config)
-      .then((response) => {
-        console.log("oi");
-        setLoading(true);
-        setUserInfo(response.data);
-      })
-      .catch((error) => {
-        navigate("/");
-        console.error(error);
-      });
-  }
 
   function GetFirstNameUser() {
     const firstName = userInfo.name.split(" ");
@@ -42,39 +16,35 @@ function Header({ loading, setLoading }) {
 
   return (
     <>
-      {loading ? (
-        <HeaderBox>
-          <Logo>
-            <img src={logo} alt="logo" />
-            <h1 onClick={() => navigate("/menu")}>
-              <span>Supermer</span>
-              <span>cash</span>
-            </h1>
-          </Logo>
-          <SearchBox>
-            <input placeholder="Pesquisar . . ." />
-          </SearchBox>
-          <Perfil>
-            {userInfo ? <GetFirstNameUser /> : <button>Entrar</button>}
-            <img
-              src="https://conteudo.imguol.com.br/c/esporte/eb/2022/09/27/neymar-comemora-gol-marcado-pela-selecao-brasileira-contra-a-tunisia-1664308063053_v2_450x600.jpg"
-              alt="PerfilPhoto"
-              onClick={() => navigate("/perfil")}
-            />
-            <FontAwesomeIcon
-              icon={faPersonRunning}
-              size="2x"
-              cursor="pointer"
-              onClick={() => {
-                localStorage.removeItem("token");
-                navigate("/");
-              }}
-            />
-          </Perfil>
-        </HeaderBox>
-      ) : (
-        ""
-      )}
+      <HeaderBox>
+        <Logo>
+          <img src={logo} alt="logo" />
+          <h1 onClick={() => navigate("/menu")}>
+            <span>Supermer</span>
+            <span>cash</span>
+          </h1>
+        </Logo>
+        <SearchBox>
+          <input placeholder="Pesquisar . . ." />
+        </SearchBox>
+        <Perfil>
+          {userInfo ? <GetFirstNameUser /> : <button>Entrar</button>}
+          <img
+            src="https://conteudo.imguol.com.br/c/esporte/eb/2022/09/27/neymar-comemora-gol-marcado-pela-selecao-brasileira-contra-a-tunisia-1664308063053_v2_450x600.jpg"
+            alt="PerfilPhoto"
+            onClick={() => navigate("/perfil")}
+          />
+          <FontAwesomeIcon
+            icon={faPersonRunning}
+            size="2x"
+            cursor="pointer"
+            onClick={() => {
+              localStorage.removeItem("token");
+              navigate("/");
+            }}
+          />
+        </Perfil>
+      </HeaderBox>
     </>
   );
 }
