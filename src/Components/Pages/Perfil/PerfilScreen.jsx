561dbe03fc032f6d806fb8f48f8e3a2921cf267a
MattGueler <mateus.gueler@gmail.com>
2022-10-01 16:58:39 -0300

    feat: Create perfil files and call perfil screen at app file

---
+++ src/Components/Pages/Perfil/PerfilScreen.jsx
@@ -0,0 +1,159 @@
+// *Hooks
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
+
+// *Components
+import {
+  Legend,
+  Main,
+  Product,
+  ProductsBox,
+  Add,
+  ProductInfo,
+  LoadingBox,
+} from "./PerfilStyle";
+import { Container } from "../../Container/ContainerStyle";
+
+// *Image
+import { useState } from "react";
+import Header from "../../Header/Header";
+import { Button } from "../../Button/ButtonSyle";
+import Footer from "../../Footer/Footer";
+import { useEffect } from "react";
+import Loading from "../../Loading/Loading";
+
+function PerfilScreen() {
+  const navigate = useNavigate();
+
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [disable, setDisable] = useState(false);
+  const [userInfo, setUserInfo] = useState("");
+
+  const advertsImages = [
+    {
+      image: "https://m.casasbahia.com.br/assets/images/casasbahia-logo.png",
+      name: "casas bahia",
+    },
+    {
+      image: "https://capitalist.com.br/wp-content/uploads/2022/05/magalu.jpg",
+      name: "Magazine Luiza",
+    },
+    {
+      image:
+        "https://newspapers-global.s3.eu-west-2.amazonaws.com/production/promotons-br/retailers/f2926253-b47f-4bd4-a357-4784eb2e783e-atacadao-logo.jpg",
+      name: "Atacadão",
+    },
+    {
+      image:
+        "https://logosmarcas.net/wp-content/uploads/2020/11/Carrefour-Logo-1972-1982.jpg",
+      name: "Carrefour",
+    },
+    {
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbiC97TXpj0leoN3xHZhYDs0Ot_JCaiZaETvgVlUy9&s",
+      name: "Carone",
+    },
+  ];
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    getAllproducts();
+    getUserInfo(token);
+  }, []);
+
+  function getUserInfo(token) {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    axios
+      .get(`http://localhost:5000/user/me`, config)
+      .then((response) => {
+        setLoading(true);
+        setUserInfo(response.data);
+      })
+      .catch((error) => {
+        navigate("/");
+        console.error(error);
+      });
+  }
+
+  function getAllproducts() {
+    const url = "http://localhost:5000/products";
+    axios
+      .get(url)
+      .then((res) => {
+        const products = res.data;
+        setProducts(products);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  function AddProductCart(name, setQuantify, token) {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const body = {
+      name,
+    };
+    axios
+      .post(`http://localhost:5000/products`, body, config)
+      .then((response) => {
+        getQuantify(name, setQuantify, token);
+      })
+      .catch((error) => {
+        navigate("/");
+        console.error(error);
+      });
+  }
+
+  function getQuantify(name, setQuantify, token) {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    axios
+      .get(`http://localhost:5000/products/quantify/${name}`, config)
+      .then((response) => {
+        const count = response.data._count;
+        setQuantify(count);
+      })
+      .catch((error) => {
+        navigate("/");
+        console.error(error);
+      });
+  }
+
+  return (
+    <>
+      {loading ? (
+        <Container>
+          <Header userInfo={userInfo} />
+          <Main>
+            
+          </Main>
+        </Container>
+      ) : (
+        <LoadingBox>
+          <Loading
+            width="100"
+            height="100"
+            color="#000000"
+            secondColor="#000000"
+          />
+        </LoadingBox>
+      )}
+    </>
+  );
+}
+
+export default PerfilScreen;
---
+++ src/Components/Pages/Perfil/PerfilStyle.jsx
@@ -0,0 +1,144 @@
+import styled from "styled-components";
+
+export const Main = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-sizing: border-box;
+  padding: 90px 20px 0 20px;
+  position: relative;
+  background-color: #d9d9d9;
+  overflow: hidden;
+
+  button {
+    width: 50%;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    box-sizing: border-box;
+  }
+
+  button:hover {
+    transform: translate(2px, 2px);
+    border: 1px solid black;
+  }
+`;
+
+export const ProductsBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 30px;
+  border-radius: 10px;
+  background-color: #ffffff;
+  box-sizing: border-box;
+`;
+
+export const Legend = styled.div`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-sizing: border-box;
+  padding: 0 10px;
+  border-bottom: 1px solid grey;
+
+  p {
+    display: flex;
+    text-align: center;
+    justify-content: center;
+    width: 100px;
+    overflow: hidden;
+    word-break: break-all;
+  }
+
+  p:first-child {
+    width: 200px;
+  }
+
+  @media (max-width: 600px) {
+    p {
+      width: 50px;
+      overflow: hidden;
+      word-break: break-all;
+    }
+
+    p:first-child {
+      display: flex;
+      flex-direction: column;
+      width: 100px;
+    }
+  }
+`;
+
+export const Product = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-sizing: border-box;
+  border-bottom: 1px solid grey;
+  padding: 20px 10px;
+
+  p {
+    width: 100px;
+    display: flex;
+    text-align: center;
+    justify-content: center;
+    word-break: break-all;
+  }
+
+  img {
+    width: 60px;
+    height: 60px;
+    margin-bottom: 10px;
+  }
+
+  @media (max-width: 600px) {
+    p {
+      width: 50px;
+    }
+  }
+`;
+
+export const ProductInfo = styled.div`
+  width: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  @media (max-width: 600px) {
+    width: 100px;
+    display: flex;
+    flex-direction: column;
+
+    p {
+      width: 100px;
+      word-break: break-all;
+    }
+  }
+`;
+
+export const Add = styled.div`
+  width: 100px;
+  display: flex;
+  justify-content: center;
+
+  @media (max-width: 600px) {
+    width: 50px;
+  }
+`;
+
+export const LoadingBox = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #ffffff;
+  z-index: 2;
+`;
