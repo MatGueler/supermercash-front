1f698c4de5f2485a14bf941d37ce2762644d3101
MattGueler <mateus.gueler@gmail.com>
2022-10-03 22:37:52 -0300

    chore: Turn box of user infos how a component

--- src/Components/Pages/Perfil/PerfilScreen.jsx
+++ src/Components/Pages/Perfil/PerfilScreen.jsx
@@ -76,42 +76,94 @@ function PerfilScreen() {
       });
   }
 
-  function AddProductCart(name, setQuantify, token) {
-    const config = {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    };
-    const body = {
-      name,
-    };
-    axios
-      .post(`http://localhost:5000/products`, body, config)
-      .then((response) => {
-        getQuantify(name, setQuantify, token);
-      })
-      .catch((error) => {
-        navigate("/");
-        console.error(error);
-      });
-  }
-
-  function getQuantify(name, setQuantify, token) {
-    const config = {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    };
-    axios
-      .get(`http://localhost:5000/products/quantify/${name}`, config)
-      .then((response) => {
-        const count = response.data._count;
-        setQuantify(count);
-      })
-      .catch((error) => {
-        navigate("/");
-        console.error(error);
-      });
+  function UpdateInfos() {
+    return (
+      <>
+        <BoxInfo>
+          <p>Nome:</p>
+          {!updateInfos ? (
+            <p>{name}</p>
+          ) : (
+            <Input
+              placeholder="Name"
+              type="text"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              value={name}
+              disabled={disable}
+            />
+          )}
+        </BoxInfo>
+        <BoxInfo>
+          <p>Email:</p>
+          {!updateInfos ? (
+            <p>{email}</p>
+          ) : (
+            <Input
+              type="email"
+              placeholder="Email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              value={email}
+              disabled={disable}
+            />
+          )}
+        </BoxInfo>
+        <BoxInfo>
+          <p>Endereço:</p>
+          {!updateInfos ? (
+            <p>{adress}</p>
+          ) : (
+            <Input
+              placeholder="Name"
+              type="text"
+              onChange={(e) => {
+                setAdress(e.target.value);
+              }}
+              value={adress}
+              disabled={disable}
+            />
+          )}
+        </BoxInfo>
+        <BoxInfo>
+          <p>Telefone:</p>
+          {!updateInfos ? (
+            <p>{phone}</p>
+          ) : (
+            <Input
+              placeholder="Telefone"
+              type="text"
+              onChange={(e) => {
+                setPhone(e.target.value);
+              }}
+              value={phone}
+              disabled={disable}
+            />
+          )}
+        </BoxInfo>
+        {!updateInfos ? (
+          <Button
+            color="grey"
+            onClick={() => {
+              setUpdateInfos(!updateInfos);
+            }}
+          >
+            Atualizar dados
+          </Button>
+        ) : (
+          <Button
+            color="blue"
+            onClick={() => {
+              setUpdateInfos(!updateInfos);
+            }}
+          >
+            Salvar dados
+          </Button>
+        )}
+      </>
+    );
   }
 
   function StackedExample() {
@@ -130,89 +182,7 @@ function PerfilScreen() {
           <Main>
             <PerfilInfo>
               <h2>Dados de cadastro</h2>
-              <BoxInfo>
-                <p>Nome:</p>
-                {!updateInfos ? (
-                  <p>{name}</p>
-                ) : (
-                  <Input
-                    placeholder="Name"
-                    type="text"
-                    onChange={(e) => {
-                      setName(e.target.value);
-                    }}
-                    value={name}
-                    disabled={disable}
-                  />
-                )}
-              </BoxInfo>
-              <BoxInfo>
-                <p>Email:</p>
-                {!updateInfos ? (
-                  <p>{email}</p>
-                ) : (
-                  <Input
-                    type="email"
-                    placeholder="Email"
-                    onChange={(e) => {
-                      setEmail(e.target.value);
-                    }}
-                    value={email}
-                    disabled={disable}
-                  />
-                )}
-              </BoxInfo>
-              <BoxInfo>
-                <p>Endereço:</p>
-                {!updateInfos ? (
-                  <p>{adress}</p>
-                ) : (
-                  <Input
-                    placeholder="Name"
-                    type="text"
-                    onChange={(e) => {
-                      setAdress(e.target.value);
-                    }}
-                    value={adress}
-                    disabled={disable}
-                  />
-                )}
-              </BoxInfo>
-              <BoxInfo>
-                <p>Telefone:</p>
-                {!updateInfos ? (
-                  <p>{phone}</p>
-                ) : (
-                  <Input
-                    placeholder="Telefone"
-                    type="text"
-                    onChange={(e) => {
-                      setPhone(e.target.value);
-                    }}
-                    value={phone}
-                    disabled={disable}
-                  />
-                )}
-              </BoxInfo>
-              {!updateInfos ? (
-                <Button
-                  color="grey"
-                  onClick={() => {
-                    setUpdateInfos(!updateInfos);
-                  }}
-                >
-                  Atualizar dados
-                </Button>
-              ) : (
-                <Button
-                  color="blue"
-                  onClick={() => {
-                    setUpdateInfos(!updateInfos);
-                  }}
-                >
-                  Salvar dados
-                </Button>
-              )}
+              <UpdateInfos />
             </PerfilInfo>
             <PerfilImage>
               <img src="https://conteudo.imguol.com.br/c/esporte/eb/2022/09/27/neymar-comemora-gol-marcado-pela-selecao-brasileira-contra-a-tunisia-1664308063053_v2_450x600.jpg" />
