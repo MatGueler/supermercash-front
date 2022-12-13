a001fe6a44f99542d8de3cc7df832bd64a85596a
MattGueler <mateus.gueler@gmail.com>
2022-12-13 01:05:07 -0300

    :pencil: feat:
    Modify apiUrl to use .env file

--- src/Components/Services/MockServices.js
+++ src/Components/Services/MockServices.js
@@ -1,2 +1,3 @@
 // export const DeployUrl = "https://supercash-deploy.herokuapp.com";
-export const DeployUrl = "http://localhost:5000";
+// export const DeployUrl = "http://localhost:5000";
+export const DeployUrl = process.env.REACT_APP_URL;
